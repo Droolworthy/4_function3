eb09c370aeb2d00574db9d5cb1ed72479b23524e
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-09-05 18:23:06 +0300

    Добавьте файлы проекта.

---
+++ 4_function3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _4_function3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberInput;
+            int number;
+            bool canExitApp = true;
+            bool result;
+
+            Console.Write("Введите номер: ");
+            numberInput = Console.ReadLine();
+
+            result = int.TryParse(numberInput, out number);
+
+            while (canExitApp)
+            {
+                if (result)
+                {
+                    Console.Write($"Число успешно сконвертировано! - {number}");
+                    canExitApp = false;
+                }
+                else
+                {
+                    Console.WriteLine("Не удалось сконвертировать число, попробуйте ещё раз.");
+                    Console.Write("Ввод: ");
+                    numberInput = Console.ReadLine();
+                    result = int.TryParse(numberInput, out number);
+                }
+            }
+        }
+    }
+}       
